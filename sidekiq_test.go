10667d0dfbed36f1ee2021853a5b40db1b9b61fd
Tim Flapper <tim.flapper@streem.com.au>
2019-05-29 12:15:07 +1000

    Put in Sidekiq tests

---
+++ sidekiq_test.go
@@ -0,0 +1,33 @@
+// Copyright (c) 2016, M Bogus.
+// This source file is part of the KUBE-AMQP-AUTOSCALE open source project
+// Licensed under Apache License v2.0
+// See LICENSE file for license information
+
+// +build integration
+
+package main
+
+import (
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestGetEnqueuedNonExistentHost(t *testing.T) {
+	_, err := getEnqueued("http://nonexistanthost/sidekiq/stats")
+	if err == nil {
+		t.Fatalf("Error expected")
+	}
+	if got, want := err.Error(), "Get http://nonexistanthost/sidekiq/stats: dial tcp: lookup nonexistanthost: no such host"; !strings.HasPrefix(got, want) {
+		t.Errorf("Expected err='%s', got: '%s'", want, got)
+	}
+}
+
+func TestEnqueued(t *testing.T) {
+	_, err := getEnqueued(sidekiqStatsURI())
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func sidekiqStatsURI() string { return os.Getenv("SIDEKIQ_STATS_URI") }
